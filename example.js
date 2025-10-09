7e2e2df1a31063eb787ecb86c15c1c2d2089565d
Suman <sumanchandu.alahari@gmail.com>
2025-10-09 20:55:17 +0530

    Added first javascript file

---
+++ example.js
@@ -0,0 +1,21 @@
+var isTrue = false;
+// iterative statements
+for (var i = 0; i < 5; i++) {
+  console.log("hello suman", i);
+  // conditional statements
+  if (i == 2) {
+    console.log("value is ture");
+  } else {
+    console.log("value is false");
+  }
+}
+
+//multi-paradigm
+// imperative way
+for (var i = 0; i < 5; i++) {
+  console.log("hello suman", i);
+}
+// declare way
+[0,1,2,3,4].forEach((i) => {
+    console.log("hello suman", i);
+})
